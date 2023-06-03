8cd4175f456bcd9cfcabb514c2178e61798d3a7b
Ardhi Harry <ardhiharry@gmail.com>
2023-06-03 22:40:28 +0700

    Menambahkan endpoint /register, /login, /users

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 import express from 'express';
 import bodyParser from 'body-parser';
-import db from './routes/database.js';
+import Register from './controllers/Register.js';
+import Login from './controllers/Login.js';
+import Users from './controllers/Users.js';
 
 const app = express();
 const port = 3000;
@@ -13,42 +15,14 @@ app.get('/', (req, res) => {
   res.send('Selamat datang di aplikasi Express.js');
 });
 
-// Endpoint "/register" untuk mendaftar pengguna baru
-app.post('/register', (req, res) => {
-  const { username, password } = req.body;
-  const user = { username, password };
+// Menggunakan controller untuk endpoint "/register"
+app.use('/register', Register);
 
-  // Menyimpan data pengguna ke database
-  db.query('INSERT INTO users SET ?', user, (err) => {
-    if (err) {
-      res.status(500).send('Gagal mendaftar pengguna');
-    } else {
-      res.status(200).send('Berhasil mendaftar pengguna');
-    }
-  });
-});
+// Menggunakan controller untuk endpoint "/login"
+app.use('/login', Login);
 
-// Endpoint "/login" untuk proses login
-app.post('/login', (req, res) => {
-  const { username, password } = req.body;
-
-  // Mengecek apakah data pengguna valid
-  db.query('SELECT * FROM users WHERE username = ?', username, (err, results) => {
-    if (err) {
-      res.status(500).send('Gagal melakukan login');
-    }
-
-    if (results.length > 0) {
-      if (results[0].password === password) {
-        res.status(200).send('Berhasil login');
-      } else {
-        res.status(401).send('Kombinasi username dan password salah');
-      }
-    } else {
-      res.status(401).send('Kombinasi username dan password salah');
-    }
-  });
-});
+// Menggunakan controller untuk endpoint "/users"
+app.use('/users', Users);
 
 // Menjalankan server
 app.listen(port, () => {
